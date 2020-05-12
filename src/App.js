87706a36ce58132e3d6118ed4040d1466f45acba
Marie Helen Berger <44783768+mie91@users.noreply.github.com>
2020-05-12 17:06:12 +0200

    Added components,assets and sass folders
    + Partials and basic variables.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./sass/style.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>Hello World</h1>
+    <p>Irure proident aute ea fugiat consequat sunt ex mollit excepteur aute proident est. Officia sunt reprehenderit sunt laboris nisi aliqua quis officia. Proident in cupidatat nisi magna dolore.</p>
+    </>
   );
 }
 
